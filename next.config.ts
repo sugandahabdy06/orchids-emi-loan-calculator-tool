a660ed4ad011ea6b4bd4bce3f282ecc522d28468
sugandahabdy06 <sugandahabdy06@gmail.com>
2026-01-09 16:55:10 +0700

    Update next.config.ts
    fix next config ts

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,15 @@
 import type { NextConfig } from "next";
 import path from "node:path";
 
-const LOADER = path.resolve(__dirname, 'src/visual-edits/component-tagger-loader.js');
+const LOADER = path.resolve(__dirname, "src/visual-edits/component-tagger-loader.js");
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-      {
-        protocol: 'http',
-        hostname: '**',
-      },
+      { protocol: "https", hostname: "**" },
+      { protocol: "http", hostname: "**" }
     ],
   },
-  outputFileTracingRoot: path.resolve(__dirname, '../../'),
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -26,10 +19,10 @@ const nextConfig: NextConfig = {
   turbopack: {
     rules: {
       "*.{jsx,tsx}": {
-        loaders: [LOADER]
-      }
-    }
-  }
+        loaders: [LOADER],
+      },
+    },
+  },
 };
 
 export default nextConfig;
