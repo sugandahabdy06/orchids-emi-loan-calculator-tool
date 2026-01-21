82ea6713b7355fd44e6dbf529ebffb39294eae3d
sugandahabdy06 <sugandahabdy06@gmail.com>
2026-01-21 13:57:28 +0700

    Update layout.tsx
    edit layout.tsx for script ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 
@@ -26,6 +27,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Google AdSense */}
+        <Script
+          id="adsense-script"
+          async
+          strategy="afterInteractive"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7704781336157313"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className="antialiased">
         <ThemeProvider
           attribute="class"
